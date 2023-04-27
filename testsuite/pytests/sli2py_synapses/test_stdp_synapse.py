654ba0b6f33aa436a1528d0536d125b8bc2fa4ce
Melissa Lober <melissa.lober@gmx.de>
2023-04-27 14:46:52 +0200

    Set parameters

--- testsuite/pytests/sli2py_synapses/test_stdp_synapse.py
+++ testsuite/pytests/sli2py_synapses/test_stdp_synapse.py
@@ -20,11 +20,43 @@
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Ensures that NEST throws an exception if one tries to create illegal gap-junction connections. 
-Furthermore it is checked that the delay cannot be set for gap-junction connections.
+A parrot_neuron that repeats the spikes from a poisson generator is
+connected to an iaf_psc_alpha that is driven by inh. and exc. poisson input.
+The synapse is an stdp_synapse. After the simulation, we go through the pre-
+and postsyn. spike-trains spike by spike and try to reproduce the STDP
+results. The final weight obtained after simulation is compared to the final
+weight obtained from the test.
 """
 
-from email import message
 import nest
 import pytest
 
+
+@pytest.fixture(autouse=True)
+def resetkernel():
+    """
+    Reset kernel to clear reset delay and resolution parameters.
+    """
+
+    nest.ResetKernel()
+
+# input parameters
+K_exc = 8000.
+K_inh = 2000.
+nu = 10.
+nu_x = 1.7
+w_exc = 45.
+w_inh = -5. * w_exc
+delay = 1.0
+axonal_delay = 0.0
+backpr_delay = delay - axonal_delay
+
+# stdp parameters
+stdp_params = {}
+stdp_params['alpha'] = 1.1
+stdp_params['lambda'] = 0.01
+stdp_params['tau_plus'] = 20.
+stdp_params['tau_minus'] = 30.
+stdp_params['mu_plus'] = 1.0
+stdp_params['mu_minus'] = 1.0
+stdp_params['Wmax'] = 2 * w_exc
