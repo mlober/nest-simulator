7f52d050b43d0f9b1b259660b13a8eba45ed7faa
Melissa Lober <melissa.lober@gmx.de>
2023-04-27 15:58:54 +0200

    Update test

--- testsuite/pytests/sli2py_synapses/test_stdp_synapse.py
+++ testsuite/pytests/sli2py_synapses/test_stdp_synapse.py
@@ -30,6 +30,7 @@ weight obtained from the test.
 
 import nest
 import pytest
+import numpy as np
 
 
 @pytest.fixture(autouse=True)
@@ -50,13 +51,86 @@ w_inh = -5. * w_exc
 delay = 1.0
 axonal_delay = 0.0
 backpr_delay = delay - axonal_delay
+tau_minus = 30.
 
 # stdp parameters
 stdp_params = {}
 stdp_params['alpha'] = 1.1
 stdp_params['lambda'] = 0.01
 stdp_params['tau_plus'] = 20.
-stdp_params['tau_minus'] = 30.
 stdp_params['mu_plus'] = 1.0
 stdp_params['mu_minus'] = 1.0
 stdp_params['Wmax'] = 2 * w_exc
+
+
+def setup_network_and_run_simulation():
+
+    # create
+    pg_exc = nest.Create('poisson_generator', {'rate': K_exc * (nu + nu_x)})
+    pg_inh = nest.Create('poisson_generator', {'rate': K_inh * nu})
+    pg_pre = nest.Create('poisson_generator', {'rate': nu})
+
+    parrot = nest.Create('parrot_neuron')
+    neuron = nest.Create('iaf_psc_alpha', {'tau_minus': tau_minus})
+
+    sr_pre = nest.Create('spike_recorder')
+    sr_post = nest.Create('spike_recorder')
+
+    # connect
+    nest.SetDefaults('stdp_synapse', stdp_params)
+    
+    nest.Connect(pg_exc, neuron, syn_spec={'weight': w_exc, 'delay': delay})
+    nest.Connect(pg_inh, neuron, syn_spec={'weight': w_inh, 'delay': delay})
+    nest.Connect(pg_pre, neuron, syn_spec={'weight': w_exc, 'delay': delay})
+
+    nest.Connect(parrot, neuron, syn_spec={'synapse_model': 'stdp_synapse', 'weight': w_exc, 'delay': delay})
+
+    nest.Connect(parrot, sr_pre)
+    nest.Connect(neuron, sr_post)
+
+
+    # simulate
+    nest.Simulate(10000.)
+
+    pre_spikes = sr_pre.events['times'] + axonal_delay
+    post_spikes = sr_pre.events['times'] + backpr_delay
+
+    final_weight = nest.GetConnections(source=parrot, target=neuron).weight
+
+    return pre_spikes, post_spikes, final_weight
+
+
+pre_spikes, post_spikes, final_weight = setup_network_and_run_simulation()
+
+# initial parameters
+K_plus = 0.
+K_minus = 0.
+last_pre = 0
+last_post = 0
+j = 0
+i = 0
+
+post_spike = post_spikes[i]
+pre_spike = pre_spikes[j]
+w = w_exc / stdp_params['Wmax']
+
+# define functions for weight update and spike times
+
+def update_K_plus(K_plus):
+    K_plus = 1.0 + K_plus * np.exp((last_pre - pre_spike) / stdp_params['tau_plus'])
+    return K_plus
+
+
+def update_K_minus(K_minus):
+    K_minus = 1.0 + K_minus * np.exp((last_pre - pre_spike) / stdp_params['tau_minus'])
+    return K_minus
+
+
+def next_post_spike():
+    i += 1
+    last_post = post_spike
+    post_spike = post_spikes[i]
+
+
+def facilitate():
+    
